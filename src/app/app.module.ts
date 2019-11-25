4f3b549f3a96c07317c2653b75ceb5f6a3eb8faa
Carlos Enrique Amador Oviedo <coviedo.ies@segurosatlas.com.mx>
2019-11-25 01:06:44 -0600

    probando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './@theme/header/header.component';
+import { NavBarComponent } from './@theme/nav-bar/nav-bar.component';
+import { ContenedorComponent } from './@theme/contenedor/contenedor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavBarComponent,
+    ContenedorComponent
   ],
   imports: [
     BrowserModule
